0c1dec7302a4a9d814ce056679fda2e37ebd5930
Deeshan Liyanage <dtliyanage27@gmail.com>
2024-11-12 00:31:30 +0530

    Add functionality to change the date based on user interaction with the calendar.

---
+++ app/src/main/java/com/example/task_app/AddTask.java
@@ -0,0 +1,73 @@
+package com.example.task_app;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Objects;
+
+public class AddTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_add_task);
+        //Create toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).show();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // Add the back button
+
+        //match the background color with the color toolbar
+        ConstraintLayout main = findViewById(R.id.main);
+        int mainColor = main.getSolidColor();
+        toolbar.setBackgroundColor(mainColor);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //Disables the text box to enter the date
+        EditText dateText = findViewById(R.id.editTextDate);
+        dateText.setText("Select Date");
+        dateText.setEnabled(false);
+        dateText.setTextColor(Color.BLACK);
+
+        //Tracks the user interaction with the calendar and changes the date field accordingly
+        CalendarView calendar = findViewById(R.id.calendarView);
+        calendar.setOnDateChangeListener(
+                new CalendarView.OnDateChangeListener() {
+                    public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                        EditText dateText = findViewById(R.id.editTextDate);
+                        CharSequence date =  dayOfMonth +"/"+ month +"/" + year;
+                        dateText.setText(date);
+                    }
+                }
+        );
+    }
+
+    public void addDate(View v){
+        CalendarView calendar = findViewById(R.id.calendarView);
+        EditText date = findViewById(R.id.editTextDate);
+        String dateC = String.valueOf(calendar.getDate());
+        Toast.makeText(this,dateC,Toast.LENGTH_LONG).show();
+        //date.setText(dateC);
+       //calendar.setDate(calendar.getDate());
+        //Log.d("presss", String.valueOf(calendar.getDate()));
+    }
+}
